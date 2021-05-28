9e445677fa5f36fa7f4e19d5d544b4b7cd5828dc
yuhang <51709956+yuhang23@users.noreply.github.com>
2021-05-28 18:07:18 +0200

    const

--- src/it/unibs/fp/rovineperdute/Team.java
+++ src/it/unibs/fp/rovineperdute/Team.java
@@ -44,7 +44,7 @@ public class Team {
      * @param distanza the distanza
      */
     public void setCarburanteTotale(double[] distanza) {
-        this.carburante_tot = distanza[distanza.length - 1];
+        this.carburante_tot = distanza[distanza.length - Costante.C1];
     }
 
     /**
@@ -70,22 +70,22 @@ public class Team {
 
         ArrayList<Citta> citta = new ArrayList(Rovina.getRovina());
 
-        for (int i = 0; i < numero_citta; i++) {
+        for (int i = Costante.C0; i < numero_citta; i++) {
             distanza[i] = Double.POSITIVE_INFINITY; // set di tutte le distanze a infinito
-            citta_precedenti[i] = -1;
+            citta_precedenti[i] = -Costante.C1;
             visitato[i] = false;
         }
 
-        distanza[citta_partenza.getId()] = 0; // la distanza per arrivare alla sorgente è 0
+        distanza[citta_partenza.getId()] = Costante.C0; // la distanza per arrivare alla sorgente è 0
 
-        for (int i = 0; i < numero_citta; i++) {
+        for (int i = Costante.C0; i < numero_citta; i++) {
             int id_attuale = distanzaPiuBreve(distanza, visitato); // va a trovare l'id della città con distanza più breve
 
             visitato[id_attuale] = true; // set la citta a true, cioè è già stata visitata
-            for (int j = 0; j < numero_citta; j++) {                                                            // condizione:
+            for (int j = Costante.C0; j < numero_citta; j++) {                                                            // condizione:
                 if (!visitato[j] &&                                                                             // 1. la citta non è stata ancora visitata
                         citta.get(id_attuale).getPercorsi().containsKey(j) &&                                   // 2. verifica se la citta è collegata con se stessa
-                        citta.get(id_attuale).getPercorsi().get(j) != 0 &&                                      // 3. la distanza non sia 0
+                        citta.get(id_attuale).getPercorsi().get(j) != Costante.C0 &&                                      // 3. la distanza non sia 0
                         !Double.isInfinite(distanza[id_attuale]) &&                                             // 4. la distanza non sia infinito
                         (distanza[id_attuale] + citta.get(id_attuale).getPercorsi().get(j)) < distanza[j]) {    // 5. se distanza + distanza prossima città è minore alla distanza presente nella tabella
 
@@ -108,9 +108,9 @@ public class Team {
     public int distanzaPiuBreve(double[] distanza, boolean[] visitato) {
 
         double min = Double.POSITIVE_INFINITY;
-        int pos = -1;
+        int pos = -Costante.C1;
 
-        for (int i = 0; i < distanza.length; i++) { // scorre l'array di distanze
+        for (int i = Costante.C0; i < distanza.length; i++) { // scorre l'array di distanze
             if (distanza[i] <= min && !visitato[i]) { // se la distanza all'i-esima posizione è minore del min allora va ad aggiornare
                 min = distanza[i];
                 pos = i;
@@ -132,11 +132,11 @@ public class Team {
      */
     public void setPercorsoMinimo(int[] citta_precedenti) {
 
-        int indice = citta_precedenti.length - 1; // parte dalle rovine perdute
+        int indice = citta_precedenti.length - Costante.C1; // parte dalle rovine perdute
 
         percorso_minimo.push(Citta.getCittaById(indice));
 
-        while (citta_precedenti[indice] != -1) { // fa fichè non arriva alla sorgente
+        while (citta_precedenti[indice] != -Costante.C1) { // fa fichè non arriva alla sorgente
             percorso_minimo.push(Citta.getCittaById(citta_precedenti[indice]));
             indice = citta_precedenti[indice];
         }
